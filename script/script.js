a9f9594fbd3bc2c8e8c191040c0fdb6341e06c6b
marco-scocca <marcoscocca1991@gmail.com>
2024-06-26 08:12:52 +0200

    insert prompt eta e number km

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let number_km = prompt('inserisci il numero di kilometri');
+let eta = prompt('inserisci eta del passeggero');
+let sconto = 0;
+if (eta)
+
+    document.getElementById('template_literal').innerText = `${first_name} ${second_name}`;
+let full_string = `${first_name} ${second_name}`;
