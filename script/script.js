1daf16cee0a1cfb94f49b733b6c74a07d82f7db8
marco-scocca <marcoscocca1991@gmail.com>
2024-06-26 14:14:57 +0200

    calcolo prezzo

--- script/script.js
+++ script/script.js
@@ -1,12 +1,8 @@
-let number_km = prompt('inserisci il numero di kilometri');
-let eta = prompt('inserisci eta del passeggero');
-let sconto = 0;
-if (eta < 18) {
-    sconto = 20;
-}
-else if (eta > 65) {
-    sconto = 40;
-}
 
-document.getElementById('template_literal').innerText = `${first_name} ${second_name}`;
-let full_string = `${first_name} ${second_name}`;
+let km = prompt('inserisci il numero di kilometri');
+
+let eta = prompt('inserisci eta del passeggero');
+
+let prezzo_base = km * 0.21;
+
+
