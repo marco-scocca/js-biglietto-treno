2cb6be05d2b60a3c45ba4c440efd64db009db7f6
marco-scocca <marcoscocca1991@gmail.com>
2024-06-26 14:19:12 +0200

    creazione classi sconto

--- script/script.js
+++ script/script.js
@@ -5,4 +5,14 @@ let eta = prompt('inserisci eta del passeggero');
 
 let prezzo_base = km * 0.21;
 
+let prezzo_finale = prezzo_base;
+
+if (eta < 18) {
+    let sconto = prezzo_base * 0.2;
+    prezzo_finale = prezzo_base - sconto;
+}
+else if (eta > 65) {
+    let sconto = prezzo_base * 0.4;
+    prezzo_finale = prezzo_base - sconto;
+}
 
