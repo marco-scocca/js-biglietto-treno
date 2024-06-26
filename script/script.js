d1c35e6ea1cff281558122597f7788a2e28806d3
marco-scocca <marcoscocca1991@gmail.com>
2024-06-26 08:17:17 +0200

    confronto età per sconto

--- script/script.js
+++ script/script.js
@@ -1,7 +1,12 @@
 let number_km = prompt('inserisci il numero di kilometri');
 let eta = prompt('inserisci eta del passeggero');
 let sconto = 0;
-if (eta)
+if (eta < 18) {
+    sconto = 20;
+}
+else if (eta > 65) {
+    sconto = 40;
+}
 
-    document.getElementById('template_literal').innerText = `${first_name} ${second_name}`;
+document.getElementById('template_literal').innerText = `${first_name} ${second_name}`;
 let full_string = `${first_name} ${second_name}`;
